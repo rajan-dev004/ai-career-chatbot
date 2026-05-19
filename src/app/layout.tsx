fcafba44960ffae7314c7d6d883e93d96a75cd8f
rajan-dev004 <rajankumawat123@gmail.com>
2026-05-19 21:55:21 +0530

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <script dangerouslySetInnerHTML={{ __html: `
           try {
